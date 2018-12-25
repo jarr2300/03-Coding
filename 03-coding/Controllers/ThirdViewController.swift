e47af5ddffc1e563c4a9a9a5e3c05ae38ce3fa35
Macabuku <jarr2300foros@gmail.com>
2018-12-25 19:22:00 +0100

    Segunda batalla del ejercicio 3 terminada

---
+++ 03-coding/Controllers/ThirdViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ThirdViewController.swift
+//  03-coding
+//
+//  Created by Macabuku on 24/12/18.
+//  Copyright © 2018 Macabuku. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- 03-codingTests/_3_codingTests.swift
+++ 03-codingTests/_3_codingTests.swift
@@ -32,3 +32,4 @@ class _3_codingTests: XCTestCase {
     }
 
 }
+
